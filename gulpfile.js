d2ae4928767d8f65d0b291b25171d856c64989f4
Joe Gesualdo <joegesualdo@gmail.com>
2016-03-09 23:10:10 -0500

    Remove react

--- gulpfile.js
+++ gulpfile.js
@@ -10,10 +10,10 @@ var jshint = require('gulp-jshint');
 var stylish = require('jshint-stylish');
 var server = require("./server.js")
 
-gulp.task('convert-jsx', function (){
+gulp.task('compile-js', function (){
   // http://egorsmirnov.me/2015/05/25/browserify-babelify-and-es6.html
-  var bundler = browserify('./src/scripts/app.jsx')
-    bundler.transform(babelify, {presets: ['react']})
+  var bundler = browserify('./src/scripts/app.js')
+    bundler.transform(babelify)
     var stream = bundler.bundle()
     return stream
       // vinle-source-stream transforms stream from browserify to 
@@ -29,21 +29,22 @@ gulp.task('compile-css', function(){
     .pipe(gulp.dest('public'));
 })
 
-gulp.task('lint', function() {
+gulp.task('lint-js', function() {
   return gulp.src('./src/scripts/**/*')
+    // TODO: Replaxce jsx hinter with javascript hinter.
     .pipe(jshint({ linter: require('jshint-jsx').JSXHINT }))
     .pipe(jshint.reporter(stylish))
 });
 
 gulp.task('watch', function(){
   gulp.watch("src/stylesheets/**/*", ['compile-css']);
-  gulp.watch("src/scripts/**/*", ['convert-jsx', 'lint']);
+  gulp.watch("src/scripts/**/*", ['compile-js', 'lint-js']);
 });
 
 gulp.task('start-server', function(){
   server.createServer()
 })
 
-gulp.task("default", ["compile-css", "convert-jsx", "watch", "start-server"], function(){
+gulp.task("default", ["compile-css", "compile-js", "watch", "start-server"], function(){
 })
 
